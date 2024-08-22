480332540dd183c103a4ebf0989b83e1ca57f31d
Pankajjr12 <67278679+Pankajjr12@users.noreply.github.com>
2024-08-22 16:01:55 +0530

    first commit

---
+++ src/main/java/com/firstspring/learning/demo/LearningPartOneApplication.java
@@ -0,0 +1,13 @@
+package com.firstspring.learning.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPartOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPartOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstspring/learning/demo/LearningPartOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspring.learning.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPartOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
